51aa0209e117bc876f6105eaf2bd4cf6c99f3f6f
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-01 10:34:03 +0200

    Configure application.rb and add devise routes sessions and registrations

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,7 +3,7 @@ class Users::SessionsController < Devise::SessionsController
 
   private 
 
-  def respond_with(_resource, options ={})
+  def respond_with(_resource, _options = {})
     render json: {
       status: {
         code: 200,
