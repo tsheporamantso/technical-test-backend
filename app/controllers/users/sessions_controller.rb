8747fe0506411af459d96804da62b4d668f66413
tsheporamantso <tshepo.ramantso@outlook.com>
2024-05-01 07:22:34 +0200

    Add user sign in logic

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,25 +1,15 @@
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  respond_to :json
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private 
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def respond_with(_resource, options ={})
+    render json: {
+      status: {
+        code: 200,
+        message: 'User signed in successfuly',
+        data: current_user
+      }
+    }, status: :ok
+  end
 end
